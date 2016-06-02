b67baf19eb6cecc3ec8a1748c66f1408674d10de
Sergey Miron <sergey.miron@icloud.com>
2016-06-02 09:24:36 +0700

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,36 @@
+/*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true, node: true */
+/*global window, console, document, $, jQuery, google */
+
+
+/**
+ * On document ready
+ */
+$(document).ready(function () {
+
+	/** Fastclick */
+	FastClick.attach(document.body);
+
+	/** Fullpage */
+	$('.page').each(function () {
+		$(this).fullpage({
+			menu: '#dots',
+			css3: true,
+			touchSensitivity: 10,
+			scrollOverflow: true,
+			keyboardScrolling: false,
+			animateAnchor: false,
+			recordHistory: false,
+			verticalCentered: true,
+			resize: false,
+			scrollingSpeed: 1000
+		});
+		/*
+		$('.down', this).on('click', function (event) {
+			$.fn.fullpage.moveSectionDown();
+			event.preventDefault();
+		});
+		*/
+		//$.fn.fullpage.setAllowScrolling(false);
+	});
+
+});
